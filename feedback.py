d31c6f3829aaf55496bf62e50196ff07cb63e54e
Amari Byrd <amaribyrd1@gmail.com>
2024-02-17 19:55:40 -0500

    implmented the n-grams for reordering the augmented query

--- feedback.py
+++ feedback.py
@@ -4,7 +4,11 @@ import sys
 import string
 import re
 
-from sklearn.feature_extraction.text import TfidfVectorizer
+## Import itertools for permutations
+from itertools import permutations
+
+## Import the sklearn library for TF-IDF weighting and bigram generation
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 ## Import the nltk library for a list of english stop-words
 import nltk
@@ -20,9 +24,9 @@ STOP_WORDS = set(stopwords.words('english'))
 
 def cmd_line():
     """
-    Relevance Feedback Function
-    Description:
-
+    Command Line Input Function
+    Description: Accept and validate command line arguments for api key, engine id, precision, and query from user
+    Returns: api key, engine id, precision, and query
     """
     # Check if the correct number of command line arguments is provided
     if len(sys.argv) != 5:
@@ -34,19 +38,23 @@ def cmd_line():
     google_engine_id = sys.argv[2]
     
     try:
+        # Validate precision
         target = float(sys.argv[3])
         if not (0 < target <= 1):
             raise ValueError("Precision must be a number greater than 0 and less than/equal to 1.")
         
         query = sys.argv[4]
 
+        # Validate query
         if not query:
             raise ValueError("Query is empty. input a query you want to search")
   
+   # catch percision and query errors
     except ValueError as e:
         print(f"Error: {e}")
         sys.exit(1)
 
+    # Return command line arguments
     return json_api_key, google_engine_id, target, query
 
 def clean_terms(text):
@@ -75,58 +83,126 @@ def clean_terms(text):
 def calculate_tf_idf(documents):
     """
     Search Function
-    Description: Calculates tf-idf score for each word in the document 
+    Description: Calculates tf-idf score for each word in the document during the relevance feedback
     Takes documents containing snippets of the api user result
     Returns term vector as a dictionary containing the tf-idf score for each word in the document ((word:tf-idf score))
+    credit: https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.TfidfVectorizer.html
+    credit: https://stackoverflow.com/questions/53294482/how-to-get-tf-idf-scores-for-the-words
     """
      
-    #calculate the idf_score for vector, use english stop words
-    vectorizer = TfidfVectorizer(analyzer='word',stop_words='english')
-    tfidf_matrix = vectorizer.fit_transform(documents)
+    # create tf-idf vectorizer to calculate tf-idf score
+    vectorizer = TfidfVectorizer()
+
+    # calculate tf-idf score for each word in the document (non-relevant, relevant, and original query)
+    tfidf_vectors = vectorizer.fit_transform(documents)
 
     # get term words
     document_word = vectorizer.get_feature_names_out()
 
-    # create term vector for all terms in the document 
-    term_vector = dict((word, idf_score) for word, idf_score in zip(document_word, tfidf_matrix.toarray()[0]))
+    # create term vector for all terms in the document containing the tf-idf score and the word
+    term_vector = dict(zip(document_word, tfidf_vectors.toarray().sum(axis=0)))
 
+    # return the term vector
     return term_vector
 
-def get_new_query(original_query,updated_query):
+def reorder_query(query,relevant_document):
     """
-    Get New Query Function
-    Description: Generate new query by getting two new words based on weights from relevance feedback and reorder them with n-grams
-    Takes original query, the current query passed to api and updated query,  results from Rocchio's algorithm 
-    Returns new query with two new words
+    Reorder Query Function
+    Description: Generate new query by creating bigrams from all permutations of the query and finding the tf-idf score for each bigram vs all relevant documents
+    Takes  query, the current query passed to api and updated query,  results from Rocchio's algorithm 
+    Returns best ordered permutation of the query, based on highest tf-idf score
     """
-    # TODO - get two new words based on updated query contains terms and weights and 
-    # find the two new words with highest tf-idf weight and add them to the original query) and find the best 
-    #reordering of the new query with n-grams sklearn library (sklearn.feature_extraction.text.CountVectorizer)
-    # and return the updated query
 
-    pass
+    # join all relevant documents into one string
+    combined_relevant_docs = ' '.join(relevant_document)
 
-def relevance_feedback(original_query, relevant_feedback, non_relevant_feedback, alpha=1, beta=0.9, gamma=.1):
+    # create count vectorizer for creating bigrams 
+    count_vectorizer = CountVectorizer(ngram_range=(2, 2))
+
+    # Generate all permutations of the query
+    query_perm = dict((' '.join(word), None) for word in permutations(query))
+
+    # create tf-idf vectorizer to calculate tf-idf score in bigrams
+    tfidf_vectorizer = TfidfVectorizer(ngram_range=(2,2))
+
+    # calculate tf-idf score for each bigram in the relevant document
+    _ = tfidf_vectorizer.fit([combined_relevant_docs])
+
+    # get term words
+    document_word = tfidf_vectorizer.get_feature_names_out()
+
+    # loop through each permutation
+    for perm in query_perm:
+        # create bigrams of the current permutation
+        perm_count_matrix = count_vectorizer.fit_transform([perm])
+        
+        # get bigram term words of the current permutation
+        bigrams = count_vectorizer.get_feature_names_out()
+       
+        # calculate tf-idf score for each bigram in the current permutation
+        perm_tdfif_vector = tfidf_vectorizer.transform(bigrams)
+     
+        # create term vector for all terms in the document containing the tf-idf score and the word
+        term_vector = dict(zip(document_word, perm_tdfif_vector.toarray().sum(axis=0)))
+       
+        # Initialize the score
+        current_score = 0
+
+        # Loop through each bigram
+        for b in bigrams:
+            # get the tf-idf score of the bigram and add it to the current score if the bigram is in the relevant document else add 0
+            current_score += term_vector.get(b,0)
+
+        # Add the current score to the dictionary [perm] to represent the weight of the permutation
+        query_perm[perm] = current_score
+
+    # sort the permutations by weight in descending order
+    sorted_query_perm = sorted(query_perm.items(), key=lambda x:x[1], reverse=True)
+
+    # Return the best permutation to be new ordered query, one with highest weight
+    return sorted_query_perm[0][0]
+
+    
+
+def relevance_feedback(original_query, relevant_feedback, non_relevant_feedback, alpha=1, beta=0.9, gamma=0.1):
     """
     Relevance Feedback Function
-    Description:
-
+    Description: implement Rocchio's algorithm for relevance feedback based on user feedback on relevant and non-relevant documents from Google Search (y/n). 
+    Calculates the updated query to improve search results by reducing irrelevant terms and increasing relevant terms.
+    Takes original query, relevant feedback, array containing all relevant documents (y), non-relevant feedback, array containing all non-relevant documents(n).
+    Consist of snippets and title from google search results.
+    Takes alpha, beta, alpha, beta, and gamma for Rocchio's algorithm (1, 0.9, 0.1)
+    Returns updated query, the new query including two new words to be used in the next search (the best query that leans more towards relevant search results)
     """
+    # Initialize the number of relevant and non-relevant docs
     relevant_docs_length = len(relevant_feedback)
     non_relevant_docs_length = len(non_relevant_feedback)
+    
+     # Clean the terms in the relevant and non-relevant feedback arrays (remove stop words, stemming) and store them in a list of terms ['per', 'se', ...]
     original_query = [term.lower() for text in original_query for term in text.split()]
-    relevant_feedback = [clean_terms(term) for text in relevant_feedback for term in text.split() if ((term not in STOP_WORDS) & (len(term) > 1) & (term not in string.punctuation) & (term != "..."))]
-    non_relevant_feedback = [clean_terms(term) for text in non_relevant_feedback for term in text.split() if ((term not in STOP_WORDS) & (len(term) > 1) & (term not in string.punctuation) & (term != "..."))]
+    
+    relevant_feedback = [clean_terms(term) for text in relevant_feedback for term in text.split() 
+                         if ((term.lower() not in STOP_WORDS) & (len(term) > 1) & (term not in string.punctuation) & (term != "..."))]
+    
+    non_relevant_feedback = [clean_terms(term) for text in non_relevant_feedback for term in text.split() 
+                             if ((term.lower() not in STOP_WORDS) & (len(term) > 1) & (term not in string.punctuation) & (term != "..."))]
+    
 
-    # Initalize dictionary for relevant term frequencies
+
+    # # Initalize dictionary for relevant term frequencies
     frequency_count = {}
 
     for term in relevant_feedback:
         # Using get method to handle the case where the word is not in the dictionary
         frequency_count[term] = frequency_count.get(term, 0) + 1
 
-
    #TODO: DELETE THIS LATER
+    #---------------------------------------------------------------------------------------------
+
+    # for term in relevant_feedback:
+    #     # Using get method to handle the case where the word is not in the dictionary
+    #     frequency_count[term] = frequency_count.get(term, 0) + 1
+
     # #Create vectors for the original query, relevant, and non-relevant feedback
     # original_query_vector = {}
     # relevant_feedback_vector = {}
@@ -140,45 +216,82 @@ def relevance_feedback(original_query, relevant_feedback, non_relevant_feedback,
 
     # for term in non_relevant_feedback:
     #     non_relevant_feedback_vector[term] = non_relevant_feedback_vector.get(term, 0) + 1
-
+    #---------------------------------------------------------------------------------------------
+        
+    # Create term vectors for the original query, relevant, and non-relevant feedback
     original_query_vector = calculate_tf_idf(original_query)
     relevant_feedback_vector = calculate_tf_idf(relevant_feedback)
     non_relevant_feedback_vector = calculate_tf_idf(non_relevant_feedback)
-
+    
     # Update the query using Rocchio's algorithm
     updated_query_vector = {}
     for term in set(original_query + relevant_feedback + non_relevant_feedback):
-
+        # Calculate the new term weight of the term using Rocchio's algorithm
         term_weight = (
-            alpha * original_query_vector.get(term, 0) 
+            alpha * original_query_vector.get(term, 0) # Original query component
             + 
-
-            beta * relevant_feedback_vector.get(term, 0) * 1 / relevant_docs_length 
+            
+            beta * relevant_feedback_vector.get(term, 0) * 1 / relevant_docs_length  #  relevant document component
 
             - 
-            gamma * non_relevant_feedback_vector.get(term, 0) * 1 / non_relevant_docs_length
+            gamma * non_relevant_feedback_vector.get(term, 0) * 1 / non_relevant_docs_length # non-relevant document component
+            
             + 
-            frequency_count.get(term,0) # give extra weight to frequent relevant terms
+            frequency_count.get(term,0) # give extra weight to frequent relevant terms 
             ) 
         
+        # Set the new term weight to 0 if it is less than 0 else set it to the new term weight (remove negative values)
         updated_query_vector[term] = max(0, term_weight)
 
-    # Sort the terms by weight in descending order
+    # Sort the terms in the updated query by weight in descending order
     sorted_terms = sorted(updated_query_vector.items(), key=lambda x: x[1], reverse=True)
+    
 
     # Take the top two terms that were not in the original query
-    top_two_terms = [(term,weight) for term, weight in sorted_terms if term not in [clean_terms(term) for term in original_query]][:2]
+    # prevent duplicates from original query and stem terms ('apples','apple') in the new query -> [term for term in original_query] + [clean_terms(term) for term in original_query]
+  
+    ### TODO: DELETE THIS LATER
+      #---------------------------------------------------------------------------------------------
+    # top_two_terms = [(term,weight) for term, weight in sorted_terms if term not in 
+    #                  ([term for term in original_query] + [clean_terms(term) for term in original_query])][:2]
+      #---------------------------------------------------------------------------------------------
 
-    # Bring in the original query terms
-    augmented_terms = top_two_terms + [(term, weight) for term, weight in sorted_terms if term in original_query]
+    top_two_terms = [term for term, _ in sorted_terms if term not in 
+                     ([term for term in original_query] + [clean_terms(term) for term in original_query])][:2]
+    
+    # join the new top two terms with original query terms to represent the new query terms
+    
+     ### TODO: DELETE THIS LATER
+      #---------------------------------------------------------------------------------------------
+    #augmented_terms = top_two_terms + [(term, weight) for term, weight in sorted_terms if term in original_query]
+      #---------------------------------------------------------------------------------------------
+    
+    augmented_terms = top_two_terms + [term for term, _ in sorted_terms if term in original_query]
+    
+    # reorder the terms to find the final augmented query using bigrams and tf-idf weighting
 
-    # Sort new query terms by their weights
-    new_sorted_terms = sorted(augmented_terms, key=lambda x: x[1], reverse=True)
+    augmented_query = reorder_query(augmented_terms, relevant_feedback)
+  
 
-    # Create the final augmented query
-    augmented_query = ' '.join([term for term, _ in new_sorted_terms])
-    new_augmented_terms = ' '.join([term for term, _ in top_two_terms])
+     ## DELETE THIS LATER
+    #---------------------------------------------------------------------------------------------
+    # # Sort new query terms by their weights
+    # new_sorted_terms = sorted(augmented_terms, key=lambda x: x[1], reverse=True)
 
+    # # Create the final augmented query
+    # #augmented_query = ' '.join([term for term, _ in new_sorted_terms])
+    #---------------------------------------------------------------------------------------------
+
+    # Combines the two new query terms (used to display to user the chosen terms in next iteration i.e. Augmented by restuarant newyork)
+    
+     ### TODO: DELETE THIS LATER
+      #---------------------------------------------------------------------------------------------
+    #new_augmented_terms = ' '.join([term for term, _ in top_two_terms])
+      #---------------------------------------------------------------------------------------------
+
+    new_augmented_terms = ' '.join(top_two_terms)
+
+    # Return the new augmented query and the augmented terms
     return new_augmented_terms, augmented_query
 
 
@@ -197,7 +310,9 @@ def search(query,target, api_key, cx, k_results=10):
     relevant_links = 0 
 
     try:
+        # Display search parameters lines to user
         query_print_cmd(api_key, cx, query, target)
+
         url = "https://www.googleapis.com/customsearch/v1"
         params = {
             "q": query,
@@ -212,8 +327,6 @@ def search(query,target, api_key, cx, k_results=10):
         # Display search results
         if 'items' in json:
           # Initiaize a list to store results
-
-          print 
           relevant_results = []
           non_relevant_results = []
 
@@ -234,7 +347,7 @@ def search(query,target, api_key, cx, k_results=10):
 
                     # Convert feedback to lower in case user enters inconsistently, remove accidental spaces
                     ## If user indicates relevant link - save title to relevant list
-                    print("feedback: ", feedback)
+                
                     if feedback.strip().lower() == 'y':
                         relevant_links += 1
                         relevant_results.append(result['title'])
@@ -276,8 +389,12 @@ def search(query,target, api_key, cx, k_results=10):
             print("Indexing results....")
             print("Generating relevance feedback...")
 
+            # Conduct relevance feedback (rocchio) to find next query and augmented terms that will be added to the previous query
             augmented_terms, augmented_query = relevance_feedback([query], relevant_results, non_relevant_results)
+
+            # Display augmented terms to user
             print(f"Augmented by {augmented_terms}")
+            
             # Recursively call search() until desired precision is reached
             search(augmented_query, target, api_key, cx)
 
@@ -297,7 +414,7 @@ def search(query,target, api_key, cx, k_results=10):
 def query_print_cmd(api_key, cx, query, percision):
     """
     Query Print CMD
-    Description: Display user input from the command line
+    Description: Display keys and current query to user (parameters and result line to be displayed before each search)
     """
     print('Parameters:')
     print(f"Client Key = {api_key}")
@@ -307,6 +424,8 @@ def query_print_cmd(api_key, cx, query, percision):
     print(f"Google Search Results:")
     print("======================")
 
+
+# Main function - start of the relevance feedback process
 def main():
     # Read in the required keys, precision, and query from command-line
     api_key, cx, target, query = cmd_line()
